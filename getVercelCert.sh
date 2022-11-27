e14047e51b7311ef77c8ae0cc06ebfee3203a440
Lean <devLeandrols@hotmail.com>
2022-11-26 23:23:12 -0300

    read shell script and get vercel cert ssl as base6r format using openssl

---
+++ getVercelCert.sh
@@ -0,0 +1,1 @@
+openssl s_client -servername vercel.app -connect vercel.app:443 | openssl x509 -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+
+const exec = require("child_process").exec;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Yo!");
+});
+
+app.get("/logs", (req, res) => {
+  try {
+    const vercelCertShellScript = exec("sh ./getVercelCert.sh");
+    vercelCertShellScript?.stdout?.on("data", (data) => {
+      return res.status(200).json({ error: null, logId: data.trim() });
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: `Internal Server Error:: ${error}`, logId: "" });
+  }
+});
+
+app.listen(process.env.PORT || 3000);
