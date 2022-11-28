426f43bd489773746e64ce3730d62bcf288b5403
Lean <devLeandrols@hotmail.com>
2022-11-28 09:21:07 -0300

    remove pkgs

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const app = express();
 
-const opensslTools = require("openssl-cert-tools");
-const openssl = require("openssl-nodejs");
-
 const { exec } = require("child_process");
-
-const spawn = require("child_process").spawn;
-const productionEnv = Object.create(process.env);
-productionEnv.NODE_ENV = "production";
-spawn("node", ["app.js"], { env: productionEnv });
 
 app.use(express.json());
 
@@ -19,23 +11,19 @@ app.get("/", (req, res) => {
 
 app.get("/logs", (req, res) => {
   try {
-    opensslTools.getCertificate("vercel.app", "443", (err, cert) => {
-      // if (err) console.error("Error getCertificate", err);
-      // else console.log(cert);
-      if (!err) {
-        res.status(200).json({ error: null, logId: cert });
-      }
+    const openssl = exec("sh ./getVercelCert.sh");
+
+    openssl?.stdout?.on("data", (data) => {
+      res.status(200).json({ error: null, logId: data.trim() });
     });
 
-    // const openssl = exec("sh ./getVercelCert.sh");
+    openssl?.stdout?.on("error", (error) => {
+      res.status(500).json({ error, logId: "" });
+    });
 
-    // openssl?.stdout?.on("data", (data) => {
-    //   res.status(200).json({ error: null, logId: data.trim() });
-    // });
+    openssl.stdin.end();
 
-    // openssl.stdin.end();
-
-    // setTimeout(() => openssl.kill(), 300);
+    setTimeout(() => openssl.kill(), 300);
   } catch (error) {
     return res
       .status(500)
