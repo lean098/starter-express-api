2e0e7be587164474ae61aea425103488aa4d740f
Lean <devLeandrols@hotmail.com>
2023-10-25 17:15:33 -0300

    Add bash command on top

--- getVercelCert.sh
+++ getVercelCert.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/bash
 openssl s_client -servername vercel.app -connect vercel.app:443 | openssl x509 -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
